eb99e2c8d9473e7005a37ede31ee48e700b1235f
Sven Duve <svenduve@gmail.com>
2020-11-02 10:57:49 +0100

    new stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import _ from 'lodash';
 import './style.css';
+import imageSineu from './hometown-221539_1280.jpg';
+import sunSet from './sunset-over-the-golf-course-644477_1280.jpg';
+import Data from './data.csv';
+import xmlData from './data.xml';   
+
 
 function component() {
     const element = document.createElement('div');
@@ -7,8 +12,26 @@ function component() {
     // lodash now imported by the script itself
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = sunSet;
   
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(xmlData);
+
     return element;
 }
 
+function addPicture() {
+    const element = document.createElement('img');
+    element.src = imageSineu;
+    element.innerHTML = "This is a picture form Sineu";
+
+    return element;
+}
+
+
 document.body.appendChild(component());
+document.body.appendChild(addPicture());
